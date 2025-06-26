720965283a2d9e54908b8129f24766ac7032a956
Sofia Gil <gx.sofia19@gmail.com>
2025-06-26 14:48:02 -0400

    new background and minor changes

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const taskName = document.querySelector('#activity');
-const dueDate = document.querySelector('#dueDate');
 const taskList = document.querySelector('.taskToDo');
 const form = document.querySelector('.addActivity');
 
 form.addEventListener('submit', function(e) {
-    const title = taskName.value;
-    const due = dueDate.value;
+    const task = taskName.value;
     const newTask = document.createElement('li');
-    newTask.innerText = title;
+    newTask.innerText = task;
     taskList.append(newTask);
     e.preventDefault();
-
 });

41c7ee98164c6c888a4150a26af6256c3e198202
Sofia Gil <gx.sofia19@gmail.com>
2025-06-26 15:31:30 -0400

    checkbox and task creation functionality implementedd

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const taskName = document.querySelector('#activity');
-const taskList = document.querySelector('.taskToDo');
+const taskList = document.querySelector('#taskToDo');
 const form = document.querySelector('.addActivity');
 
 form.addEventListener('submit', function(e) {
+    e.preventDefault();
     const task = taskName.value;
     const newTask = document.createElement('li');
     newTask.innerText = task;
     taskList.append(newTask);
-    e.preventDefault();
+
+    const checkBtn = document.createElement('input');
+    checkBtn.setAttribute('type', 'checkbox');
+    newTask.append(checkBtn);
+
+    const delBtn = document.createElement('button');
+    checkBtn.innerHTML('x');
+    newTask.append(delBtn);
 });

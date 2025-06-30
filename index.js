20799a0c02860845a21ef8ac1b9d4930b320ce27
Sofia Gil <gx.sofia19@gmail.com>
2025-06-30 08:50:37 -0400

    more edits

--- index.js
+++ index.js
@@ -2,24 +2,33 @@ const taskName = document.querySelector('#activity');
 const taskList = document.querySelector('#taskToDo');
 const form = document.querySelector('.addActivity');
 
-const deleteBtn = document.querySelector(delBtn);
+// const deleteBtn = document.querySelector(delBtn);
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     const task = taskName.value;
     const newTask = document.createElement('li');
     newTask.innerText = task;
-    taskList.append(newTask);
+    // taskList.append(newTask);
 
     const checkBtn = document.createElement('input');
     checkBtn.setAttribute('type', 'checkbox');
-    newTask.append(checkBtn);
+    // newTask.append(checkBtn);
 
     const delBtn = document.createElement('button');
-    const del = document.createTextNode('x');
-    delBtn.append(del);
-    newTask.append(delBtn);
+    delBtn.innerText = 'x';
+
+    delBtn.addEventListener('click', delTask);
+    // delBtn.append(del);
+    // newTask.append(delBtn);
+
+    newTask.append(checkBtn, delBtn);
+    taskList.append(newTask);
 
     taskName.value = '';
 });
+
+function delTask(newTask) {
+    taskList.remove(newTask);
+};
 

93847701d751388593196cc406c17322a73a8130
Sofia Gil <gx.sofia19@gmail.com>
2025-06-30 09:19:33 -0400

    fixed remove functionality to remove single chosen element. js to order elements in order I desire.

--- index.js
+++ index.js
@@ -2,33 +2,28 @@ const taskName = document.querySelector('#activity');
 const taskList = document.querySelector('#taskToDo');
 const form = document.querySelector('.addActivity');
 
-// const deleteBtn = document.querySelector(delBtn);
-
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     const task = taskName.value;
     const newTask = document.createElement('li');
     newTask.innerText = task;
-    // taskList.append(newTask);
 
     const checkBtn = document.createElement('input');
     checkBtn.setAttribute('type', 'checkbox');
-    // newTask.append(checkBtn);
 
     const delBtn = document.createElement('button');
     delBtn.innerText = 'x';
 
     delBtn.addEventListener('click', delTask);
-    // delBtn.append(del);
-    // newTask.append(delBtn);
 
-    newTask.append(checkBtn, delBtn);
+    newTask.insertBefore(checkBtn, newTask.firstChild);
+    newTask.append(delBtn);
     taskList.append(newTask);
 
     taskName.value = '';
 });
 
-function delTask(newTask) {
-    taskList.remove(newTask);
+function delTask(e) {
+    e.target.parentElement.remove();
 };
 
